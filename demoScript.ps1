1bdb24db8164122f7d0e741620d017bda81baacc
Bart Delanghe <badelang@microsoft.com>
2020-07-29 18:16:29 +0200

    Added ha config file

--- demoScript.ps1
+++ demoScript.ps1
@@ -7,10 +7,23 @@ $tfdir="../deploy/terraform"
 
 terraform init $tfdir
 
-terraform plan --var-file=singleHANA.json $tfdir
+terraform plan --var-file=singleHANARepo.json $tfdir
 
 terraform apply --var-file=singleHANARepo.json  --auto-approve $tfdir
 
 terraform destroy --var-file=singleHANARepo.json --auto-approve $tfdir
 
+-------------------
+ssh into rti to run ansible-playbook
+ssh azureadm@{outputIP}
+
 ansible-playbook -i hosts.yml ~/sap-hana/deploy/ansible/sap_playbook.yml
+
+-------------------
+After completion: ssh into HANA DB
+ssh bdelangh@10.97.1.10 (check host for internal ip address)
+sudo su - hn1adm
+HDB info
+
+-------------------
+
